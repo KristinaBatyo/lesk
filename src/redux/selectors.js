2f19b7551f47d8d1acb0f70b28460b7f20e639d1
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2023-05-25 22:54:02 +0300

    fix

--- src/redux/selectors.js
+++
@@ -1,4 +0,0 @@
-export const getContacts = state => state.contacts.items;
-export const getLoading = state => state.contacts.isLoading;
-export const getError = state => state.contacts.error;
-export const getFilter = state => state.filters;
